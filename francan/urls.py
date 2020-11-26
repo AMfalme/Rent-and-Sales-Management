3899d233f0742d710c8ab744c638af78ebe810cb
GMfalme <gmfalme@mizizi.io>
2020-11-27 01:12:28 +0300

    Create modelPages and add context variables to pass to respectful templates i.e. with context Homepage, ClientPage, ShelvesPage, ProductPage without context - CustomerPage, SalesPage, RentPage Also create models necessary for the data we will be capturing from the users in models.Model i.e Client, Shelf, Product(Stockin) ,ProductSale(Stockout), RentPayment Also link Shelf, Client, ProductSale, Product, RentPayment to the admin site for easy control
    avoid commiting DS_Store files in gitignore
    
    Add urls to the added model pages as well to the viewPages
    
    Add shelf_page template, shelves_page Template

--- francan/urls.py
+++ francan/urls.py
@@ -7,6 +7,7 @@ from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
+from home import views as custom_views
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
@@ -15,6 +16,19 @@ urlpatterns = [
     path('documents/', include(wagtaildocs_urls)),
 
     path('search/', search_views.search, name='search'),
+    path('shelf/<int:id>/', custom_views.shelf, name="shelf"),
+    path('<int:id>/add_product/', custom_views.addproduct,
+         name="add Shelf Product"),
+    path('add_product/', custom_views.newproduct),
+    path('sell_product/<int:id>', custom_views.sellitem),
+    path('handle_product_sale/', custom_views.handle_product_sale),
+    path('add_new_client/', custom_views.add_client),
+    path('add_shelf/', custom_views.add_shelf),
+    path('add_rent/', custom_views.add_rent),
+    path('rent_payments/', custom_views.rent_payments),
+    path('addClient/', custom_views.handle_add_client),
+    path('addShelf/', custom_views.handle_add_shelf),
+    path('addRent/', custom_views.handle_add_rent),
 
 ]
 
@@ -25,7 +39,8 @@ if settings.DEBUG:
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
--- home/models.py
+++ home/models.py
@@ -1,59 +1,131 @@
 from django.db import models
-
+from django.contrib import admin
 from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    pass
+
+    def get_context(self, request):
+        context = super(HomePage, self).get_context(request)
+        context['info'] = Shelf.objects.all()
+        return context
 
 
 class ShelvesPage(Page):
-    pass
+    def get_context(self, request):
+        context = super(ShelvesPage, self).get_context(request)
+        context['info'] = Shelf.objects.all()
+        return context
 
 
 class ShelfPage(Page):
     pass
 
 
-class ShelfModel(models.Model):
+class ClientsPage(Page):
+    def get_context(self, request):
+        context = super(ClientsPage, self).get_context(request)
+        context['info'] = Client.objects.all()
+        return context
+
+
+class CustomersPage(Page):
+    pass
+
+
+class SalesPage(Page):
+    pass
+
+
+class RentsPage(Page):
+    pass
+
+
+class ProductsPage(Page):
+    def get_context(self, request):
+        context = super(ProductsPage, self).get_context(request)
+        context['products'] = Product.objects.all()
+        return context
+
+
+class Client(models.Model):
+    "This model defines the abstract shelf client table and the necessary details for each Client"
+
+    def __str__(self):
+        return self.client_name
+    client_name = models.CharField(max_length=255)
+    client_id_number = models.IntegerField()
+    client_business_name = models.CharField(max_length=255)
+    business_type = models.CharField(max_length=200)
+    photo = models.ImageField(
+        upload_to='images', default="avatar.png")
+    date = models.DateTimeField(auto_now_add=True)
+    client_contacts = models.CharField(max_length=255)
+    client_status = models.BooleanField(default=True)
+    client_email = models.CharField(max_length=255)
+
+
+class Shelf(models.Model):
     "This model defines the abstract shelf table and the necessary details for each shelf"
-    shelf_number = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return ('shelf number: '+str(self.shelf_number))
+    shelf_number = models.CharField(unique=True, max_length=255)
     shelf_size = models.CharField(max_length=255)
     shelf_price = models.CharField(max_length=60)
     shelf_description = models.TextField()
+    client_owner = models.ForeignKey(
+        Client, on_delete=models.DO_NOTHING, blank=True, null=True)
 
 
-class ClientModel(models.Model):
-    "This model defines the abstract shelf client table and the necessary details for each Client"
-
-    client_name = models.CharField(max_length=255)
-    client_id_number = models.IntegerField()
-    client_business_name = models.CharField(max_length=255)
-    client_contacts = models.CharField(max_length=255)
-    client_status = models.CharField(max_length=255)
-    client_email = models.CharField(max_length=255)
-
-
-class CustomerModel(models.Model):
-    "This model defines the abstract shelf client table and the necessary details for each Client"
-    client_name = models.CharField(max_length=255)
-    client_id_number = models.IntegerField()
-    client_business_name = models.CharField(max_length=255)
-    client_contacts = models.CharField(max_length=255)
-    client_email = models.CharField(max_length=255)
-
-
-class ProductModel(models.Model):
+class Product(models.Model):
     "This model defines the abstract Products client bring to Francan and the necessary details for each Product"
+
+    def __str__(self):
+        return self.product_name
     product_name = models.CharField(max_length=255)
+    photo = models.ImageField(
+        upload_to='images', default='images/product1.jpg')
     product_description = models.TextField()
-    product_item_price = models.CharField(max_length=255)
+    product_unit_price = models.CharField(max_length=255)
+    no_of_products = models.IntegerField()
     product_type = models.CharField(max_length=255)
+    product_brand = models.CharField(max_length=255)
+    product_shelf = models.ForeignKey(
+        Shelf, on_delete=models.CASCADE)
 
 
-class ClientStockModel(models.Model):
-    productmodel = models.ForeignKey(ProductModel, on_delete=models.CASCADE)
-    client_model = models.ForeignKey(ClientModel, on_delete=models.CASCADE)
-    product_quantity = models.CharField(max_length=255)
-    product_description = models.TextField(max_length=255)
-    no_of_items = models.IntegerField()
+Payment_choices = [
+    ("Paid to owner", 'Paid to owner'),
+    ("Cash payment", 'Cash Payment'),
+]
+
+
+class ProductSale(models.Model):
+
+    def __str__(self):
+        return self.product_id.product_name
+    product_id = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    type_of_payment = models.CharField(max_length=20, choices=Payment_choices)
+    no_of_items = models.CharField(max_length=20)
+    customer_name = models.CharField(max_length=20)
+    customer_id = models.CharField(max_length=20)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class RentPayment(models.Model):
+    def __str__(self):
+        return self.client_name + ' payment'
+
+    shelf_id = models.ForeignKey(Shelf, on_delete=models.DO_NOTHING)
+    client_id = models.CharField(max_length=255)
+    amount_paid = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    client_name = models.CharField(max_length=255)
+
+
+admin.site.register(Shelf)
+admin.site.register(Client)
+admin.site.register(ProductSale)
+admin.site.register(Product)
+admin.site.register(RentPayment)
